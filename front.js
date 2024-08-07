70c9457a8eeea73d7a16ffa5468ca58b71378c94
VijayNarasingam <vijaynarasingam12@gmail.com>
2024-08-07 15:54:11 +0530

    contact update

--- front.js
+++ front.js
@@ -6,26 +6,6 @@
  var tabcontents = document.getElementsByClassName("tab-contents");
 
 
- const scriptURL = 'https://script.google.com/macros/s/AKfycbw2_5IDy-ERIndeHoW0_pnIfRhd2YADpvu8rQhcjykSjK8g_S-5rG79JByjhmxjI8og3g/exec'
-        const form = document.forms['submit-to-google-sheet']
-        const msg = document.getElementById("msg")     
-
-
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-              msg.innerHTML = "Message Sent Successfully"
-              setTimeout(function(){
-                msg.innerHTML = ""
-              },5000)
-              form.reset()
-            })
-            .catch(error => console.error('Error!', error.message))
-        })
-
-
-
 
 
  function opentab(tabname) {
