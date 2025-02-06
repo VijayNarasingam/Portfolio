2d0b4480387c4c7b87ae1bb07a9bf528384b3f91
VijayNarasingam <vijaynarasingam12@gmail.com>
2025-02-06 15:25:05 +0530

    first commit

--- work.js
+++ work.js
@@ -6,7 +6,7 @@ let products = {
         id:5,
         productName: "Toy Shop",
         category: "Web_App",
-        image: "img/toy-shop.png",
+        image: "img/Toy-store.jpg",
         detail: "toy store web site",
         link:"https://vijaynarasingam.github.io/Toy-Store",
       },
@@ -24,7 +24,7 @@ let products = {
         id:3,
         productName: "Tic Tac Toe",
         category: "Web_App",
-        image: "img/Tic Tac Toe.png",
+        image: "img/tic tac toe1.jpg",
         detail: "Tic Tac Toe Game",
         link:"https://vijaynarasingam.github.io/Tic-Tac-Toe",
       },
@@ -33,7 +33,7 @@ let products = {
         id:2,
         productName: "Yugan Photography",
         category: "Web_page",
-        image: "img/yugan.png",
+        image: "img/yugan.jpg",
         detail: "Photographer web page",
         link:"https://vijaynarasingam.github.io/yugan_photography/",
       },
@@ -42,7 +42,7 @@ let products = {
         id:1,
         productName: "DreamEscape Travels",
         category: "Web_page",
-        image: "img/1.png",
+        image: "img/DreamEscapeTravels.png",
         detail: "Travels web Page",
         link:"https://vijaynarasingam.github.io/DreamEscapeTravels/",
       },
@@ -93,60 +93,3 @@ let products = {
     document.getElementById("products").appendChild(card);
   }
   
-  //parameter passed from button (Parameter same as category)
-  function filterProduct(value) {
-    //Button class code
-    let buttons = document.querySelectorAll(".button-value");
-    buttons.forEach((button) => {
-      //check if value equals innerText
-      if (value.toUpperCase() == button.innerText.toUpperCase()) {
-        button.classList.add("active");
-      } else {
-        button.classList.remove("active");
-      }
-    });
-  
-    //select all cards
-    let elements = document.querySelectorAll(".card");
-    //loop through all cards
-    elements.forEach((element) => {
-      //display all cards on 'all' button click
-      if (value == "all") {
-        element.classList.remove("hide");
-      } else {
-        //Check if element contains category class
-        if (element.classList.contains(value)) {
-          //display element based on category
-          element.classList.remove("hide");
-        } else {
-          //hide other elements
-          element.classList.add("hide");
-        }
-      }
-    });
-  }
-  
-  //Search button click
-  document.getElementById("search").addEventListener("click", () => {
-    //initializations
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card");
-  
-    //loop through all elements
-    elements.forEach((element, index) => {
-      //check if text includes the search value
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        //display matching card
-        cards[index].classList.remove("hide");
-      } else {
-        //hide others
-        cards[index].classList.add("hide");
-      }
-    });
-  });
-  
-  //Initially display all products
-  window.onload = () => {
-    filterProduct("all");
-  };
